735b773106704552a544df2d8ba35963546c9db1
adug0007 <adug0007@student.monash.edu>
2025-09-03 19:17:20 +1000

    Iteration 1: updated few options in the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -40,9 +40,9 @@
           <li><a href="#" :class="{active: currentRoute==='financial-literacy'}" @click.prevent="go('financial-literacy')">Financial Basics</a></li>
           <li><a href="#" :class="{active: currentRoute==='budget-planner'}" @click.prevent="go('budget-planner')">Budget Planner</a></li>
           <li><a href="#" :class="{active: currentRoute==='savings-goal-calculator'}" @click.prevent="go('savings-goal-calculator')">Savings Goals</a></li>
-          <li><a href="#" :class="{active: currentRoute==='cost-estimator'}" @click.prevent="go('cost-estimator')">Study & Living Costs</a></li>
+       <!--   <li><a href="#" :class="{active: currentRoute==='cost-estimator'}" @click.prevent="go('cost-estimator')">Study & Living Costs</a></li> -->
           <li><a href="#" :class="{active: currentRoute==='tax-learn'}" @click.prevent="go('tax-learn')">Taxes</a></li>
-          <li><a href="#" :class="{active: currentRoute==='mortgage-calculator'}" @click.prevent="go('mortgage-calculator')">Home & Mortgage Planning</a></li>
+      <!--    <li><a href="#" :class="{active: currentRoute==='mortgage-calculator'}" @click.prevent="go('mortgage-calculator')">Home & Mortgage Planning</a></li> -->
         </ul>
       </nav>
     </div>
